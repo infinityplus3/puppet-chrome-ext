cbaf9596045c9e6af1e581c743582aff17da41ce
infinityplus3 <38089107+infinityplus3@users.noreply.github.com>
2020-10-02 15:37:22 -0700

    Add files via upload
    weoeoeoeoeeoeoeoeoe

--- chrome-ext/content.js
+++ chrome-ext/content.js
@@ -1,6 +1,14 @@
 console.log("HI I EXIST LEL")
+
 let currentel = null
 let remover = null
+
+const elementAttributes = (elem) =>
+  elem.getAttributeNames().reduce((attrMap, name) => {
+    attrMap[name] = elem.getAttribute(name);
+    return attrMap;
+  }, {});
+
 const getOffset = el => {
     var _x = 0;
     var _y = 0;
@@ -13,16 +21,30 @@ const getOffset = el => {
 }
 
 const createDiv = (top, left, info, element) => {
-    let divy = document.createElement("div")
-    divy.style.top = `${top + 20}px`
-    divy.style.left = `${left}px`
-    divy.style.position = "absolute"
-    divy.style.width = "370px"
-    divy.style.height = '154px'
-    divy.style.background = "#C4C4C4"
-    divy.style["border-radius"] = "30px"
-    divy.id = `div${top}${left}`
-    document.body.appendChild(divy)
+  console.log(elementAttributes(element))
+  let divy = document.createElement("div")
+  divy.style.top = `${top + 20}px`
+  divy.style.left = `${left}px`
+  divy.style.position = "absolute"
+  divy.style.width = "370px"
+  divy.style.height = '154px'
+  divy.style.background = "#C4C4C4"
+  divy.style["border-radius"] = "30px"
+  divy.id = `div${top}${left}`
+  if (info !== []) {
+    info.forEach(item => {
+      let node = document.createElement("span")
+      node.innerHTML = item
+      divy.appendChild(node)
+    });
+  }
+  else {
+    let node = document.createElement("span")
+    node.innerHTML = "No attributes found."
+    divy.appendChild(node)
+  }
+  document.body.appendChild(divy)
+  return divy
 }
 
 const func = () => {
@@ -36,9 +58,8 @@ const func = () => {
           remover.parentNode.removeChild(remover)
         }
         let pos = getOffset(element)
-        createDiv(pos.top, pos.left, ["HAHAHHAHAHAHA"], element)
+        remover = createDiv(pos.top, pos.left, Object.values(elementAttributes(element)), element)
         currentel = element
-        remover = document.getElementById(`div${pos.top}${pos.left}`)
       }
       break
     }
@@ -47,10 +68,13 @@ const func = () => {
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendresp){
   console.log(request)
-  if (request.running === true) {
+  if (request.data.running === true) {
     document.addEventListener("mousemove", func, false)
   }
-  else if (request.running === false) {
+  else if (request.data.running === false) {
     document.removeEventListener("mousemove", func, false)
+    if (remover !== null) {
+      remover.parentNode.removeChild(remover)
+    }
   }
 })
--- chrome-ext/popup.js
+++ chrome-ext/popup.js
@@ -1,14 +1,24 @@
-let running = false
-document.getElementById("showme").addEventListener("click", async function() {
+function button(){
+  chrome.storage.local.get(["running"], function(runnin){
+    let running = !runnin.running
+    console.log(running)
     chrome.tabs.query({
         active: true,
         currentWindow: true
     }, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {
             data: {
-              running: !running
+              running: running
             }
         });
-        running = !running
     });
-});
+    chrome.storage.local.set({running: running}, function(){
+      console.log(running)
+    })
+  })
+}
+
+chrome.storage.local.set({
+  running: false
+})
+document.getElementById("startbtn").onclick = button
